3f020def3e00c8de66639c27eb5690fbd5f0706f
cfrikke <143611879+cfrikke@users.noreply.github.com>
2023-08-30 08:40:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-function areyouanIdiot() {
-let x = window.confirm("Epilepsy Warning");
-  if(x == true){
-  window.open("https://piv.pivpiv.dk/?scrlybrkr=2c3135df");
-}
+function fullScreen() {
+window.requestFullscreen();
 }
